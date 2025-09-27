cca2ebd71c027cf8d01f5e4457d4a072ea78b25a
pialcaja <pialcaja@gmail.com>
2025-09-27 08:52:05 -0500

    Paquete repository

---
+++ back/ProyectoSistemaEducativo/src/main/java/com/edusistem/repository/HorarioRepository.java
@@ -0,0 +1,19 @@
+package com.edusistem.repository;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.edusistem.model.Dia;
+import com.edusistem.model.Horario;
+
+public interface HorarioRepository extends JpaRepository<Horario, Long> {
+
+	List<Horario> findByDiaHorario(Dia dia);
+	
+	@Query("SELECT h FROM tb_horario h WHERE h.horaInicioHorario >= :inicio AND h.horaFinHorario <= :fin")
+	List<Horario> buscarEntreHoras(@Param("inicio") LocalTime inicio, @Param("fin") LocalTime fin);
+}
