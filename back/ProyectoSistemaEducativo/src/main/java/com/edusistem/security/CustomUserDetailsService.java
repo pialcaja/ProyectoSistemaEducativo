ee5737916f6fc29216e2d8fba9c5db6f716d832e
pialcaja <pialcaja@gmail.com>
2025-10-13 18:43:35 -0500

    CRUD - Usuario, Administrador, Alumno, Docente

--- back/ProyectoSistemaEducativo/src/main/java/com/edusistem/security/CustomUserDetailsService.java
+++ back/ProyectoSistemaEducativo/src/main/java/com/edusistem/security/CustomUserDetailsService.java
@@ -14,21 +14,21 @@ import com.edusistem.repository.AdministradorRepository;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private final AdministradorRepository adminRepo;
-	
-	public CustomUserDetailsService(AdministradorRepository adminRepo) {
-		this.adminRepo = adminRepo;
-	}
-	
-	@Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Administrador admin = adminRepo.findByEmailAdministrador(email)
-            .orElseThrow(() -> new UsernameNotFoundException("Admin no encontrado: " + email));
-        
-        return new org.springframework.security.core.userdetails.User(
-            admin.getEmailAdministrador(),
-            admin.getPwdAdministrador(),
-            List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
-        );
-    }
+//	private final AdministradorRepository adminRepo;
+//	
+//	public CustomUserDetailsService(AdministradorRepository adminRepo) {
+//		this.adminRepo = adminRepo;
+//	}
+//	
+//	@Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        Administrador admin = adminRepo.findByEmailAdministrador(email)
+//            .orElseThrow(() -> new UsernameNotFoundException("Admin no encontrado: " + email));
+//        
+//        return new org.springframework.security.core.userdetails.User(
+//            admin.getEmailAdministrador(),
+//            admin.getPwdAdministrador(),
+//            List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+//        );
+//    }
 }
