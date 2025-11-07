ff01b49cb6ffef2ef73681bf9ff1065a9e2c2ed7
pialcaja <pialcaja@gmail.com>
2025-11-06 20:45:28 -0500

    Componentes - Admin, Alumno, Docente

---
+++ front/ProyectoSistemaEducativo/src/app/components/alumno-panel/alumno-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumno-panel',
+  imports: [],
+  templateUrl: './alumno-panel.component.html',
+  styleUrl: './alumno-panel.component.css'
+})
+export class AlumnoPanelComponent {
+
+}
